752456967de35c859387d0f8f27950f7e3ca9a12
s-leger <stephen@3dservices.ch>
2020-05-25 14:39:54 +0200

    Update blender_pip.py
    Fix for 
    https://developer.blender.org/T76993

--- blender_pip.py
+++ blender_pip.py
@@ -35,8 +35,22 @@ class Pip:
 
     def __init__(self):
         self._ensurepip()
+    
+    @staticmethod
+    def _ensure_user_site_package():
+        import os
+        import site
+        site_package = site.getusersitepackages()
+        if os.path.exists(site_package):
+            if site_package not in sys.path:
+                sys.path.append(site_package)
+        else:
+            site_package = bpy.utils.user_resource('SCRIPTS', "site_package", create=True)
+            site.addsitedir(site_package)
 
     def _cmd(self, action, options, module):
+        if options in not None and "--user" in options:
+            self._ensure_user_site_package()
 
         cmd = [PYPATH, "-m", "pip", action]
 
